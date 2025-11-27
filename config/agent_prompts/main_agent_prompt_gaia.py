0f3ae258d78d124609da63b38bd2e8bebac00d6b
mereith <wanglu@mereith.com>
2025-11-27 22:28:32 +0800

    fix: 调整日期 prompt

--- config/agent_prompts/main_agent_prompt_gaia.py
+++ config/agent_prompts/main_agent_prompt_gaia.py
@@ -29,7 +29,9 @@ IMPORTANT IDENTITY REMINDER:
 
 In this environment you have access to a set of tools you can use to answer the user's question. 
 
-You only have access to the tools provided below. You can only use one tool per message, and will receive the result of that tool in the user's next response. You use tools step-by-step to accomplish a given task, with each tool-use informed by the result of the previous tool-use. Today is: {formatted_date}
+You only have access to the tools provided below. You can only use one tool per message, and will receive the result of that tool in the user's next response. You use tools step-by-step to accomplish a given task, with each tool-use informed by the result of the previous tool-use. 
+
+Today is: {formatted_date}. For time-dependent questions, answer based on the world as it would reasonably be today.
 
 # Tool-Use Formatting Instructions 
 
